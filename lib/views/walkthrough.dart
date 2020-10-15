617bd2fe8b86c265f98203893bfaee016840a281
valentin garnier <valentin.garnier@epitech.eu>
2020-10-16 00:20:49 +0200

    Add splashscreen + assets with logo and fonts

---
+++ lib/views/walkthrough.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Walkthrough extends StatefulWidget {
+  const Walkthrough({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _WalkthroughState createState() => _WalkthroughState();
+}
+
+class _WalkthroughState extends State<Walkthrough> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
